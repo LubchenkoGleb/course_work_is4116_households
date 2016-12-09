d0a4ca563fbe5cbc7735ec746b132377e8e4378e
Gleb <gleb.lubchenko@gmail.com>
2016-12-09 13:07:32 +0200

    crud operations for teachers and subjects

---
+++ src/main/java/com/kpi/model/LoadType.java
@@ -0,0 +1,7 @@
+package com.kpi.model;
+
+/**
+ * Created by gleb on 09.12.16.
+ */
+public class LoadType {
+}
---
+++ src/main/java/com/kpi/service/LoadTypeService.java
@@ -0,0 +1,7 @@
+package com.kpi.service;
+
+/**
+ * Created by gleb on 09.12.16.
+ */
+public class LoadTypeService {
+}
